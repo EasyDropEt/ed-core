8140d222443184f6d94b214ddc81834571797532
ffekirnew <ffekirnew0808@gmail.com>
2025-05-07 09:16:07 +0300

    Update create delivery job dto

--- src/ed_core/application/features/delivery_job/dtos/create_delivery_job_dto.py
+++ src/ed_core/application/features/delivery_job/dtos/create_delivery_job_dto.py
@@ -5,14 +5,10 @@ from uuid import UUID
 from ed_domain.core.entities.route import WayPoint
 
 
-class CreateRouteDto(TypedDict):
+class CreateDeliveryJobDto(TypedDict):
+    order_ids: list[UUID]
     waypoints: list[WayPoint]
     estimated_distance_in_kms: float
     estimated_time_in_minutes: int
-
-
-class CreateDeliveryJobDto(TypedDict):
-    order_ids: list[UUID]
-    route: CreateRouteDto
     estimated_payment: float
     estimated_completion_time: datetime
