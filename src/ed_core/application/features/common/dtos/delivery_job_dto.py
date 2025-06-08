a5a7563a70399af40aa204519b31321b8a015bca
ffekirnew <ffekirnew0808@gmail.com>
2025-06-08 15:22:41 +0300

    fix: Minor changes

--- src/ed_core/application/features/common/dtos/delivery_job_dto.py
+++ src/ed_core/application/features/common/dtos/delivery_job_dto.py
@@ -7,7 +7,6 @@ from ed_domain.core.aggregate_roots.delivery_job import (DeliveryJob,
 from ed_domain.core.value_objects.money import Money
 from pydantic import BaseModel
 
-from ed_core.application.features.common.dtos.driver_dto import DriverDto
 from ed_core.application.features.common.dtos.waypoint_dto import WaypointDto
 
 
@@ -16,7 +15,7 @@ class DeliveryJobDto(BaseModel):
     waypoints: list[WaypointDto]
     estimated_distance_in_kms: float
     estimated_time_in_minutes: int
-    driver: Optional[DriverDto]
+    driver: Optional[UUID]
     status: DeliveryJobStatus
     estimated_payment: Money
     estimated_completion_time: datetime
@@ -36,7 +35,7 @@ class DeliveryJobDto(BaseModel):
             ],
             estimated_distance_in_kms=delivery_job.estimated_distance_in_kms,
             estimated_time_in_minutes=delivery_job.estimated_time_in_minutes,
-            driver=DriverDto(**delivery_job.driver.__dict__),
+            driver=delivery_job.driver_id,
             status=delivery_job.status,
             estimated_payment=delivery_job.estimated_payment,
             estimated_completion_time=delivery_job.estimated_completion_time,
