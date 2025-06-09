14232465627ef676cc8525b530ef40a8c2fa42cc
ffekirnew <ffekirnew0808@gmail.com>
2025-06-10 01:14:40 +0300

    feat: Add email templating feature

--- src/ed_core/infrastructure/api/api_handler.py
+++ src/ed_core/infrastructure/api/api_handler.py
@@ -1,16 +1,19 @@
 from ed_auth.documentation.api.abc_auth_api_client import ABCAuthApiClient
+from ed_auth.documentation.api.auth_api_client import AuthApiClient
 from ed_notification.documentation.api.abc_notification_api_client import \
     ABCNotificationApiClient
+from ed_notification.documentation.api.notification_api_client import \
+    NotificationApiClient
 
 from ed_core.application.contracts.infrastructure.api.abc_api import ABCApi
+from ed_core.common.typing.config import Config
 
 
 class ApiHandler(ABCApi):
-    def __init__(
-        self, auth_api: ABCAuthApiClient, notification_api: ABCNotificationApiClient
-    ) -> None:
-        self._auth_api = auth_api
-        self._notification_api = notification_api
+    def __init__(self, config: Config) -> None:
+        self._auth_api = AuthApiClient(config["auth_api"])
+        self._notification_api = NotificationApiClient(
+            config["notification_api"])
 
     @property
     def auth_api(self) -> ABCAuthApiClient:
